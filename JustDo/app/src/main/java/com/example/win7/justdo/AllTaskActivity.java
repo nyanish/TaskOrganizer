d9504c7d3e1c62bff6b4620c453b8333bcdf23bc
nyanish <vanya.santeva@gmail.com>
2016-03-19 16:12:20 +0200

    Clean start

---
+++ JustDo/app/src/main/java/com/example/win7/justdo/AllTaskActivity.java
@@ -0,0 +1,13 @@
+package com.example.win7.justdo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_task);
+    }
+}
